23643cf4d5a2ed7beb78169e34394300ce7eb04f
ChrisLeePin <nobleelement@yahoo.com.sg>
2019-01-24 16:02:00 +0800

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+  var open = false;
+
+});	
+
+function display(clicked_id){
+    $("#panel" + clicked_id).slideToggle("slow");
+};
+
+
+
+function spin(){
+  var icon = document.getElementById('s3');
+  
+  if(open){
+      icon.className = 'skills_gi sk3';  
+    } else{
+      icon.className = 'skills_gi sk3 open';
+}
+        open = !open;
+}

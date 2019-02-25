a5943a7c4a7547e9b75095431f644f7c10c7bb3f
Chris <nobleelement@yahoo.com.sg>
2019-02-25 11:07:34 +0800

    Changes to the Nav and all pages

---
+++ README.txt
@@ -0,0 +1,15 @@
+Chris Lee Pin Portfolio Website
+
+Intro
+
+This website was created for the purpose of displaying my projects.
+
+Regarding the skills page. Always include different class in the boxes for ease of access and removal of CSS.
+
+The default resolution
+
+URL
+
+Author
+
+Chris Lee Pin
--- js/script.js
+++ js/script.js
@@ -1,16 +1,32 @@
-var open = false;
+$(document).ready(function(){
+	$(".hov2").hover(function(){
+	  $(this).animate({ margin: -10, width: "+=20", height: "+=20" });
+	  }, function(){
+	  $(this).animate({ margin: 0, width: "-=20", height: "-=20" });
+	});
+});
 
 function display(clicked_id){
     $("#panel" + clicked_id).slideToggle("slow");
 };
 
-function spin(){
-  var icon = document.getElementById('s3');
-  
-  if(open){
-      icon.className = 'skills_gi sk3';  
-    } else{
-      icon.className = 'skills_gi sk3 open';
+function spin(id){
+   var icon = document.getElementById(id);
+   
+	if(icon.className == ("skills_gi spin")){
+      icon.className = 'skills_gi spun';  
+	} 
+	else{
+      icon.className = 'skills_gi spin';
+	}
 }
-    open = !open;
+
+function displayNav() {
+	var x = document.getElementById("nav");
+
+  if (x.className === "navbar") {
+    x.className += "resp";
+	} else {
+    x.className = "navbar";
+  }
 }
